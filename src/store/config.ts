bc9eab5b182ca570e4032e0046a300c79839900d
lihao <15502354225@163.com>
2024-01-25 18:12:42 +0800

    修改了一些配置代码

--- src/store/config.ts
+++ src/store/config.ts
@@ -2,44 +2,38 @@ const config = {
   wx: {
     dev: {
       appid: "wx529fadf983b0b19e",
-      // BASE_URL: "https://video.hbcyszw.cn/api/axRb5g1ZQNjlqpBM/",
       BASE_URL: "https://video.test.jixuejima.com/api/dskafdsl/",
       // BASE_URL: "https://video.test.jixuejima.com/api/dsadfs/",
       WEB_VIEW: "https://video.hbcyszw.cn/api/"
     },
     test: {
       appid: "wx529fadf983b0b19e",
-      // BASE_URL: "https://video.hbcyszw.cn/api/axRb5g1ZQNjlqpBM/",
       BASE_URL: "https://video.test.jixuejima.com/api/dskafdsl/",
       WEB_VIEW: "https://video.hbcyszw.cn/api/"
     },
     prod: {
       appid: "wx529fadf983b0b19e",
       BASE_URL: "https://video.hbcyszw.cn/api/axRb5g1ZQNjlqpBM/",
-      // BASE_URL: "https://video.test.jixuejima.com/api/dskafdsl/",
       WEB_VIEW: "https://video.hbcyszw.cn/api/"
     },
   },
   tt: {
     dev: {
       appid: "wx529fadf983b0b19e",
-      // BASE_URL: "http://video.hbcyszw.cn/api/axRb5g1ZQNjlqpBM/",
       BASE_URL: "https://video.test.jixuejima.com/api/dskafdsl/",
       WEB_VIEW: "https://video.hbcyszw.cn/api/"
     },
     test: {
       appid: "wx529fadf983b0b19e",
       BASE_URL: "http://video.hbcyszw.cn/api/axRb5g1ZQNjlqpBM/",
-      // BASE_URL: "https://video.test.jixuejima.com/api/dskafdsl/",
       WEB_VIEW: "https://video.hbcyszw.cn/api/"
     },
     prod: {
       appid: "wx529fadf983b0b19e",
       BASE_URL: "http://video.hbcyszw.cn/api/axRb5g1ZQNjlqpBM/",
-      // BASE_URL: "https://video.test.jixuejima.com/api/dskafdsl/",
       WEB_VIEW: "https://video.hbcyszw.cn/api/"
     },
   },
 };
 // 抖音小程序
-export const env = config["wx"]["prod"];
+export const env = config["wx"]["dev"];
